b3bed09ee01f244b7fe765656c13c8e7e2327f33
basalganglia <ilkka.kosunen@gmail.com>
2018-06-09 17:31:22 +0300

    finished finger 10

--- finger.py
+++ finger.py
@@ -1,11 +1,14 @@
 from twisted.internet import protocol, reactor, defer, utils, endpoints
 from twisted.protocols import basic
+from twisted.web import client
+
 
 class FingerProtocol(basic.LineReceiver):
     def lineReceived(self, user):        
         d = self.factory.getUser(user)
         
         def onError(err):
+            print("not found")
             return 'internal error in server'
         d.addErrback(onError)
         
@@ -20,13 +23,12 @@ class FingerProtocol(basic.LineReceiver):
 class FingerFactory(protocol.ServerFactory):
     protocol = FingerProtocol
     
-   # def __init__(self, users):
-   #     self.users = users
+    def __init__(self, prefix):
+        self.prefix = prefix        
         
     def getUser(self, user):
-        return utils.getProcessOutput(b"finger", [user])
-#        return utils.getProcessOutput(b"C:/work/TwistedFinger/dir.exe", [user])
-    
+        return client.getPage(self.prefix + user)
+        
 fingerEndpoint = endpoints.serverFromString(reactor, "tcp:1079")
-fingerEndpoint.listen(FingerFactory())
+fingerEndpoint.listen(FingerFactory(prefix=b'http://livejournal.com/~'))
 reactor.run()
