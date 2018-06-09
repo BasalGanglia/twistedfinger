81561ad804ce2115ff46d2d7cf7e1fddbd630772
basalganglia <ilkka.kosunen@gmail.com>
2018-06-09 14:11:35 +0300

    Finger07 before deferreds

--- finger.py
+++ finger.py
@@ -1,17 +1,25 @@
-from twisted.internet import protocol, reactor, endpoints
+from twisted.internet import protocol, reactor, defer, endpoints
 from twisted.protocols import basic
 
 class FingerProtocol(basic.LineReceiver):
-    def lineReceived(self, user):
-        self.transport.write(b"no such user\r\n")
+    def lineReceived(self, user):        
+        self.transport.write(self.factory.getUser(user) + b"\r\n")
+        print("we received from user: {}", user)
         self.transport.loseConnection()
         
-    def connectionMade(self):
-        self.transport.loseConnection()
+  #  def connectionMade(self):
+   #     self.transport.loseConnection()
 
 class FingerFactory(protocol.ServerFactory):
     protocol = FingerProtocol
     
+    def __init__(self, users):
+        self.users = users
+        
+    def getUser(self, user):
+        return self.users.get(user, b"no such user")
+    
 fingerEndpoint = endpoints.serverFromString(reactor, "tcp:1079")
-fingerEndpoint.listen(FingerFactory())
-reactor.run()    
+fingerEndpoint.listen(FingerFactory({b'moshez' : b'happy and well'}))
+print("where is this even printing...")
+reactor.run()
